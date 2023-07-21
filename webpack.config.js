86383ef89982165be23885070126b09687b63778
Paul Kallarackel <paulkallarackel@gmail.com>
2023-07-21 18:51:38 +0100

    initial files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: {
+	  main: "./src/index.js",
+	  styles: "./src/styles.scss",
+	},
+	output: {
+	  path: path.resolve(__dirname, "dist"),
+	  filename: "[name].js", // Use [name] placeholder for dynamic filenames
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/index.html"),
+			filename: "index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(css|scss)$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			  },
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+};
