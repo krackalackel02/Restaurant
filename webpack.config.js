d722967b95fd9fc600f1a265eebbaa24fea47591
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-07-23 17:51:18 +0100

    inital cover page

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,34 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: {
-	  main: "./src/index.js",
-	  styles: "./src/styles.scss",
-	},
-	output: {
-	  path: path.resolve(__dirname, "dist"),
-	  filename: "[name].js",
-	  clean:true
-	   // Use [name] placeholder for dynamic filenames
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "src/index.html"),
-			filename: "index.html",
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(css|scss)$/i,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			  },
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
+  entry: {
+    bundle: ["./src/index.js", "./src/styles.scss"], // Use a single entry point that includes both JS and SCSS
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", // Output a single bundle file
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
